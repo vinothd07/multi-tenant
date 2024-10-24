485b390582ffce8680be1f291e15be448ae58af5
Vinoth Kumar <dvinothkumar63@gmail.com>
2024-10-24 17:07:06 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "public.blob.vercel-storage.com" },
+      { hostname: "res.cloudinary.com" },
+      { hostname: "abs.twimg.com" },
+      { hostname: "pbs.twimg.com" },
+      { hostname: "avatar.vercel.sh" },
+      { hostname: "avatars.githubusercontent.com" },
+      { hostname: "www.google.com" },
+      { hostname: "flag.vercel.app" },
+      { hostname: "illustrations.popsy.co" },
+      {
+        protocol: "https",
+        hostname: "www.orbis.com.ar",
+        port: "",
+        pathname: "/wp-content/themes/barberry/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
